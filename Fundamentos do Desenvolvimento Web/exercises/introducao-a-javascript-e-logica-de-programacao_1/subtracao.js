48c16bbbacd073d0af9f44919387565bfad3d151
Lucas Silva <martssilvapro@gmail.com>
2020-11-25 17:52:11 -0300

    programa 1 e 2 prontos

---
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_1/subtracao.js
@@ -0,0 +1,14 @@
+/* 
+Faça cinco programas, um para cada operação aritmética básica. 
+Seu programa deve ter duas variáveis, a e b, definidas no começo 
+com os valores que serão operados. Faça programas para:
+*/
+
+// Subtração: 
+
+let a = 5;
+let b = 5;
+
+let subtracao = a - b;
+
+console.log('O valor da subtração de a - b é: ' + subtracao);
