8f8c6ff24abcf9ed84660034a2a28e703d485b64
Lucas Silva <martssilvapro@gmail.com>
2020-11-25 18:02:41 -0300

    programa de modulo pronto

---
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_1/modulo.js
@@ -0,0 +1,14 @@
+/* 
+Faça cinco programas, um para cada operação aritmética básica. 
+Seu programa deve ter duas variáveis, a e b, definidas no começo 
+com os valores que serão operados. Faça programas para:
+*/
+
+// Módulo
+
+let a = 10;
+let b = 5;
+
+let modulo = a % b;
+
+console.log('O módulo é: ' + modulo);
