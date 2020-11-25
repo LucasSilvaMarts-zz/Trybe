a259bc29cf63de37192327b9f2bb16638ccd26e4
Lucas Silva <martssilvapro@gmail.com>
2020-11-25 18:38:19 -0300

    exercicio 4 pronto

---
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio_4.js
@@ -0,0 +1,12 @@
+/*Faça um programa que, dado um valor definido numa variável, retorne "positive" 
+se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.*/
+
+let valor = 7;
+
+if (valor > 0) {
+  console.log('Positive')
+} else if (valor < 0) {
+  console.log('Negative')
+} else {
+  console.log('Zero')
+}
