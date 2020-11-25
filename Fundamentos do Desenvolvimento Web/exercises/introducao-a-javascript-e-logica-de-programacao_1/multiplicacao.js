1b71647de0e5269dd8b816188b9d274554991fd5
Lucas Silva <martssilvapro@gmail.com>
2020-11-25 17:54:47 -0300

    programa de multiplicacao pronto

---
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_1/multiplicacao.js
@@ -0,0 +1,14 @@
+/* 
+Faça cinco programas, um para cada operação aritmética básica. 
+Seu programa deve ter duas variáveis, a e b, definidas no começo 
+com os valores que serão operados. Faça programas para:
+*/
+
+// Multiplicação
+
+let a = 4;
+let b = 4;
+
+let multiplicacao = a * b;
+
+console.log('O resultado da multiplicação de a x b é: ' + multiplicacao);
