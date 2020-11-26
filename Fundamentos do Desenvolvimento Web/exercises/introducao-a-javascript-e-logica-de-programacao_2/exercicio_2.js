8d3fd7fafb59496d92f15f82361cb329a2af26d8
Lucas Silva <martssilvapro@gmail.com>
2020-11-26 19:00:14 -0300

    exercicio corrigido

--- Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_2.js
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_2.js
@@ -7,10 +7,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let soma = 0;
 
-for(let valor in numbers) {
-  soma += numbers[valor]
+for(let i = 0; i < numbers.length; i += 1) {
+  soma += numbers[i]
 }
 
 console.log(soma);
-
-// como funciona o for in?
