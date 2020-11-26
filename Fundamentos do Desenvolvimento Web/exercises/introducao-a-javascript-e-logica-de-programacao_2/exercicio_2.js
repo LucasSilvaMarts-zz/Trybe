2f737b20d7eb0c0db75ac77243fe1934533645a5
Lucas Silva <martssilvapro@gmail.com>
2020-11-26 17:58:30 -0300

    exercicio 2 feito

---
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_2.js
@@ -0,0 +1,14 @@
+/* 
+Para o segundo exercício, você deve somar todos os valores 
+contidos no array e imprimir o resultado;
+*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+
+for(let i in numbers) {
+  soma += numbers[i]
+}
+
+console.log(soma);
