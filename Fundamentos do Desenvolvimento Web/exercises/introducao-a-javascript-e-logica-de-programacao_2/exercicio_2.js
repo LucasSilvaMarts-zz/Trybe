a34c184ce518b8e6ed3c72cb58deba362fe6bdd1
Lucas Silva <martssilvapro@gmail.com>
2020-11-27 15:12:39 -0300

    algoritmo criado

--- Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_2.js
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_2.js
@@ -12,3 +12,12 @@ for(let i = 0; i < numbers.length; i += 1) {
 }
 
 console.log(soma);
+
+/*
+  1: criar o array
+  2: criar a variável que vai guardar os ítens do array iniciando com valor 0
+  3: percorrer o array com o for
+  4: incrementar a variável com o valor correspondente a cada loop 
+  5: somar o valor da variável iniciada com 0 com os ítens do array 
+  6: imprimir o valor da soma
+*/
