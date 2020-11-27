c2d2681d6d3852875e12c389f029131dea3e67c0
Lucas Silva <martssilvapro@gmail.com>
2020-11-27 15:08:35 -0300

    algoritmo criado

--- Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_1.js
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_1.js
@@ -6,3 +6,13 @@ função console.log() ;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 console.log(numbers)
+
+// Algoritmo 
+
+/*  
+
+1°: Criar o array
+2°: Percorrer o array 
+3/ Imprimir os valores
+
+*/
