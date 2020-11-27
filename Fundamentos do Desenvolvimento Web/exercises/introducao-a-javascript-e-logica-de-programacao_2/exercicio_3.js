f535af4f250dea42ee9d7f7a9997df089db8724e
Lucas Silva <martssilvapro@gmail.com>
2020-11-27 15:18:54 -0300

    algoritmo 3 criado

--- Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_3.js
+++ Fundamentos do Desenvolvimento Web/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_3.js
@@ -15,3 +15,11 @@ for(let i = 0; i < numbers.length; i += 1) {
 
 console.log(soma / numbers.length)
 
+/* 
+  1: criar o array 
+  2: criar uma variável iniciada em 0
+  3: percorrer o array com o for
+  4: incrementar a variável com os ítens do array e somando eles
+  5: usar a variável de incremento e dividir pelo tamanho do array
+  6: imprimir o resultado usando o console.log
+*/
