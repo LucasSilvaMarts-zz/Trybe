cae8a773558a779746165d3cb0cf9941e9bee946
Lucas Silva <martssilvapro@gmail.com>
2020-12-09 11:41:28 -0300

    adiciona funcao finalizada

--- Fundamentos do Desenvolvimento Web/exercises/javascript-dom-eventos-e-web-storage_3/script.js
+++ Fundamentos do Desenvolvimento Web/exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -20,5 +20,21 @@ createDaysOfTheWeek();
 // Cria os dias do calendário
 function calendarDays() {
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysNumber = document.querySelector('#days');
+
+  for(let index = 0; index < dezDaysList.length; index += 1) {
+    const days = dezDaysList[index];
+    const dayListItem = document.createElement('li'); 
+    
+    dayListItem.classList.add('days'); // Adiciona a classe days em todos os li
+    dayListItem.innerHTML = days; 
+
+    daysNumber.appendChild(dayListItem);
+  };
   
 };
+
+calendarDays();
+
+
+
