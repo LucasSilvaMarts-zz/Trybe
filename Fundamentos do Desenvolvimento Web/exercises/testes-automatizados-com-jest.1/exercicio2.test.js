03cb8dab039782d0873144b438bcb4215c2cb3e6
Lucas Silva <martssilvapro@gmail.com>
2021-02-03 19:47:01 -0300

    exercicio 3 feito

--- Fundamentos do Desenvolvimento Web/exercises/testes-automatizados-com-jest.1/exercicio2.test.js
+++ Fundamentos do Desenvolvimento Web/exercises/testes-automatizados-com-jest.1/exercicio2.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+const { TestScheduler } = require("jest");
 
 function myRemove(arr, item) {
   let newArr = [];
@@ -11,3 +11,22 @@ function myRemove(arr, item) {
 }
 
 // implemente seus testes aqui
+
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+
+test('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+  expect(myRemove([1, 2, 3, 4], 3)).toEqual([1,2,4]);
+});
+
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+
+test('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+  expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+});
+
+// Verifique se o array passado por parâmetro não sofreu alterações
+
+test('Verifique se o array passado por parâmetro não sofreu alterações', () => {
+  expect(myRemove([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+});
+
