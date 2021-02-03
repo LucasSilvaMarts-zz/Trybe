26cdbe9578753216fe8196860d99d91fad3b6b08
Lucas Silva <martssilvapro@gmail.com>
2021-02-03 19:02:26 -0300

    teste 2 e 3 feitos

--- Fundamentos do Desenvolvimento Web/exercises/testes-automatizados-com-jest.1/exercicio1.test.js
+++ Fundamentos do Desenvolvimento Web/exercises/testes-automatizados-com-jest.1/exercicio1.test.js
@@ -14,3 +14,15 @@ function sum(a, b) {
 test('sums two values', () => {
   expect(sum(4, 5)).toEqual(9);
 });
+
+// Teste se o retorno de sum(0, 0) é 0
+
+test('sums 0 + 0 = 0', () => {
+  expect(sum(0, 0)).toEqual(0);
+});
+
+// Teste se a função sum lança um erro quando os parametros são 4 e "5" (string 5)
+
+test('parameter must to be a number', () => {
+  expect(() => { sum(4, '5') }).toThrow();
+ });
